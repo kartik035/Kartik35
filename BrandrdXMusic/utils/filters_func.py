b7ce8e996e4fb40a71373782ca0312b8a8a82968
Kartiknishad36 <164469009+Kartiknishad36@users.noreply.github.com>
2024-12-01 00:29:45 +0530

    Update filters_func.py

--- BrandrdXMusic/utils/filters_func.py
+++ BrandrdXMusic/utils/filters_func.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
-from BrandrdXMusic import app
+from kaalXMusic import app
 from pyrogram.types import InlineKeyboardMarkup, Message
-from BrandrdXMusic.utils.msg_types import button_markdown_parser
-from BrandrdXMusic.utils.notes_func import NoteFillings
+from kaalXMusic.utils.msg_types import button_markdown_parser
+from kaalXMusic.utils.notes_func import NoteFillings
 from emojis import decode
 from pyrogram.types import Message
 
