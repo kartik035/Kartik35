7ef5fd669e79148e90dcc49601b9f3f254d20f39
Kartiknishad36 <164469009+Kartiknishad36@users.noreply.github.com>
2024-12-01 22:25:47 +0530

    Update language.py

--- BrandrdXMusic/plugins/tools/language.py
+++ BrandrdXMusic/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.database import get_lang, set_lang
-from BrandrdXMusic.utils.decorators import (ActualAdminCB, language, languageCB)
+from kaalXMusic import app
+from kaalXMusic.utils.database import get_lang, set_lang
+from kaalXMusic.utils.decorators import (ActualAdminCB, language, languageCB)
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
