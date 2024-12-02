1391c46c170a18fa3e030bf3c4bb36a293cf74d8
Kartiknishad36 <164469009+Kartiknishad36@users.noreply.github.com>
2024-12-02 14:16:59 +0530

    Update live.py

--- BrandrdXMusic/plugins/play/live.py
+++ BrandrdXMusic/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from BrandrdXMusic import YouTube, app
-from BrandrdXMusic.utils.channelplay import get_channeplayCB
-from BrandrdXMusic.utils.decorators.language import languageCB
-from BrandrdXMusic.utils.stream.stream import stream
+from kaalXMusic import YouTube, app
+from kaalXMusic.utils.channelplay import get_channeplayCB
+from kaalXMusic.utils.decorators.language import languageCB
+from kaalXMusic.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
